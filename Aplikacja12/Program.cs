e4af61c356921e24e289dc69f80e1cc4502be3de
s16707 <s16707@pjwstk.edu.pl>
2025-03-09 22:54:34 +0100

    Add project files.

---
+++ Aplikacja12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aplikacja12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
